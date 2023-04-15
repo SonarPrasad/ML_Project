692514699afceadf62bcf20ceb3d35ae35c42718
SonarPrasad <prasadsonar09@gmail.com>
2023-04-15 16:35:53 +0530

    Data Tranfomation

--- source/exception.py
+++ source/exception.py
@@ -10,6 +10,8 @@ def error_message_detail(error, error_detail:sys):
         str(error)
     )
 
+    return error_message
+
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
